455bec505c57cb3f035f41d019fa05e6cfb42b84
Vajahath Ahmed <vajuoff.1@gmail.com>
2017-07-17 05:58:27 +0530

    making tests right

--- tests/index.spec.js
+++ tests/index.spec.js
@@ -4,8 +4,8 @@ const expect = require('chai').expect;
 const cv = require('../src/index');
 
 describe('testing current-version', () => {
-    it('should return 1.1.0 while checking', done => {
-        expect(cv()).to.equal('1.1.0');
+    it('should return 1.0.0 while checking', done => {
+        expect(cv()).to.equal('1.0.0');
         done();
     });
 });
