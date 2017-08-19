7604b38b462d7e67e78f7440c4642c4445a79821
Vajahath Ahmed <vajuoff.1@gmail.com>
2017-08-19 15:26:04 +0530

    logic implementation

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,17 @@
-export default function(a: number, b: number) {
-	return a + b;
+import * as rootPath from 'app-root-path';
+
+interface InterfacePackage {
+	version: string;
 }
+
+export = () => {
+	let p: InterfacePackage;
+
+	try {
+		p = rootPath.require('package.json');
+	} catch (err) {
+		throw new Error(err);
+	}
+
+	return p.version;
+};
--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -1,10 +1,15 @@
-import {expect} from 'chai';
-import add from '../src/index';
+/* tslint:disable: no-unused-expression arrow-parens */
 
-describe('Testing addition', ()=>{
-    it('returns 11 for 3,8', done=>{
-        let p = add(3,8);
-        expect(p).to.equal(11)
-        done();
-    })
-})
+import * as chai from 'chai';
+import version = require('../src/index');
+
+const expect = chai.expect;
+
+describe('Testing installed version', () => {
+	it('returns current version of this package', done => {
+		const v = version();
+		expect(v).not.to.be.undefined;
+		expect(v).to.equal('1.0.0');
+		done();
+	});
+});
