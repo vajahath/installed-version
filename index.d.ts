ca7f85c7f7bbcd36aa6042f1cb2b52cf932622d8
Vajahath Ahmed <vajuoff.1@gmail.com>
2017-08-19 16:41:39 +0530

    adds definition file

---
+++ index.d.ts
@@ -0,0 +1,13 @@
+// Type definitions for [installed-version] [1.0.0]
+// Project: [installed-version]
+// Definitions by: Vajahath Ahmed <http://twitter.com/vajahath7>
+
+/*~ This is the module template file for function modules.
+ *~ You should rename it to index.d.ts and place it in a folder with the same name as the module.
+ *~ For example, if you were writing a file for "super-greeter", this
+ *~ file should be 'super-greeter/index.d.ts'
+ */
+
+export = InstalledVersion;
+
+declare function InstalledVersion(): string;
